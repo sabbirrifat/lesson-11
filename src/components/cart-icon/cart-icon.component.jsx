53e3b8cdc36ea15f43d5ec3efacef5d763efa28c
Sabbir Rifat <rifat234dgh@gmail.com>
2020-09-18 07:35:38 +0600

    Finish the redux setup plus cart items component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,8 +4,6 @@ import { connect } from 'react-redux';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-import toggleCartHidden from '../../redux/cart/cart-action';
-
 
 import './cart-icon.styles.scss';
 
@@ -21,7 +19,7 @@ function CartIcon({toggleCartHidden}) {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        toggleCartHidden: () => dispatch(toggleCartHidden())
+        toggleCartHidden: () => dispatch({ type: 'TOGGLE_CART_HIDDEN' })
     }
 }
 
